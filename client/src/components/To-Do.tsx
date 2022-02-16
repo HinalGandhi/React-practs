d5946b4a70695e93413736a4ca4455b8ee621b66
“Hinal <hinal.g@simformsolutions.com>
2022-02-16 16:02:22 +0530

    Added functionality on + button

--- client/src/components/To-Do.tsx
+++ client/src/components/To-Do.tsx
@@ -4,40 +4,66 @@ import DateBar from "./DateBar";
 import TodoList from "./List";
 import Addactivity from "./Addactivity";
 
-class Feature extends React.Component<any>{
-  render(): JSX.Element{
-  return (
-    <>
-      <App />
-      <div className="main ">
-        <div className="container pb-5 mt-5 ">
-          <div className="row justify-content-center">
-            <div className="col-sm-3 col-md-6 col-lg-4">
-              <div className="card fw-bold  box_style">
-                <div className="m-5">
-                  <DateBar />
-                  <div className="col">
-                    <TodoList />
-                    <Addactivity/>
-                    <div className="position-relative d-flex justify-content-center">
-                      <button className="position-absolute text-muted btn_Add " >
-                        +
-                      </button>
+class Feature extends React.Component<{}, any> {
+  constructor(props: {}) {
+    super(props);
+    this.state = { isToggleOn: true };
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+    this.escFunction = this.escFunction.bind(this);
+  }
+  handleClick() {
+    this.setState((prevState: { isToggleOn: any }) => ({
+      isToggleOn: !prevState.isToggleOn,
+    }));
+  }
+  escFunction(event: { key: string }) {
+    if (event.key === "Escape") {
+      this.setState(() => ({
+        isToggleOn: true,
+      }));
+    }
+  }
+  componentDidMount() {
+    document.addEventListener("keydown", this.escFunction, false);
+  }
+
+  render(): JSX.Element {
+    return (
+      <>
+        <App />
+        <div className="main ">
+          <div className="container pb-5 mt-5 ">
+            <div className="row justify-content-center">
+              <div className="col-sm-3 col-md-6 col-lg-4">
+                <div className="card fw-bold  box_style">
+                  <div className="m-5">
+                    <DateBar />
+                    <div className="col list">
+                      <TodoList />
                     </div>
+                    {this.state.isToggleOn ? <></> : <Addactivity />}
+                    <div className="position-relative d-flex justify-content-center">
+                        <button
+                          className="position-absolute text-muted btn_Add "
+                          onClick={this.handleClick}
+                          style={{
+                            display: this.state.isToggleOn ? "block" : "none",
+                          }}
+                        >
+                          +
+                        </button>
+                      </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
   }
 }
 
 export default Feature;
-function setState(arg0: boolean) {
-  throw new Error("Function not implemented.");
-}
-
