eab879fadf0668d2f5bea38750f7c8f45481454f
“Hinal <hinal.g@simformsolutions.com>
2022-03-02 15:49:22 +0530

    Rendered userprofilecard conditionally

--- client/src/components/features/records/recordList.tsx
+++ client/src/components/features/records/recordList.tsx
@@ -9,12 +9,15 @@ import { useState } from "react";
 
 export function PostsList(): JSX.Element {
   const posts = useSelector((state: RootState) => state.posts);
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null);
   const renderedPosts = posts.map((post, index): JSX.Element => {
     return (
       <tr key={post.id} className="d-flex align-items-center">
-        <td className="d-flex userDetails" onMouseEnter={() => setUser(post)}
-          onMouseLeave={() => setUser(null)}>
+        <td
+          className="d-flex userDetails"
+          onMouseEnter={React.useCallback(() => setUser(post), [setUser])}
+          onMouseLeave={() => setUser(null)}
+        >
           <picture>
             <img
               className="profileImg img-fluid img-thumbnail rounded-circle p-0"
@@ -30,43 +33,46 @@ export function PostsList(): JSX.Element {
           </div>
         </td>
         <td className="text-success" style={{ width: "14rem" }}>
-          {index === 0 ? "Acitve" :
+          {index === 0 ? (
+            "Active"
+          ) : (
             <select className="selectMenu form-select">
               <option value="Inactive">Inactive</option>
               <option value="Active">Active</option>
             </select>
-          }
+          )}
         </td>
         <td className="" style={{ width: "10rem" }}>
-          {index === 0 ? "Owner" :
+          {index === 0 ? (
+            "Owner"
+          ) : (
             <select className="selectMenu form-select">
               <option value="Inactive">Manager</option>
               <option value="Active">Read</option>
             </select>
-          }
+          )}
         </td>
         <td>
-          {index === 0 ? <Icon.Lock
-            size={18}
-            style={{ color: "rgba(0, 0, 0, 0.6)" }}
-          /> :
-            <Icon.Trash2
-              size={18}
-              style={{ color: "rgba(0, 0, 0, 0.6)" }}
-            />
-          }
+          {index === 0 ? (
+            <Icon.Lock size={18} style={{ color: "rgba(0, 0, 0, 0.6)" }} />
+          ) : (
+            <Icon.Trash2 size={18} style={{ color: "rgba(0, 0, 0, 0.6)" }} />
+          )}
         </td>
-      </tr >
+      </tr>
     );
   });
 
   return (
-    <div className="d-flex align-items-center container bg-white mt-5">
-      <table className="table table-borderless bg-white mr-4" style={{ maxWidth: "50%" }}>
+    <div className="d-flex flex-wrap align-items-center container bg-white mt-5">
+      <table
+        className="table table-borderless bg-white mr-4"
+        style={{ maxWidth: "50%" }}
+      >
         <Navbar />
         <tbody className="posts-list">{renderedPosts}</tbody>
       </table>
-      <UserProfileCard user={user} />
+      {user && <UserProfileCard user={user} />}
     </div>
   );
 }
